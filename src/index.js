aec1aa18c97c6e79ddb56d95f330e8c6bbc38bb3
Sanjay-Kumar-262004 <sanjaykumar.ece26@gmail.com>
2024-09-22 16:17:55 +0530

    Backend Added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const app = express();
+const port = process.env.PORT || 3000;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+app.use(bodyParser.json());
+
+app.post('/bfhl', (req, res) => {
+  const { data, file_b64 } = req.body;
+  const alphabets = [];
+  const numbers = [];
+  let highestLowercase = null;
+
+  data.forEach(item => {
+    if (!isNaN(item)) {
+      numbers.push(item);
+    } else if (/^[a-zA-Z]$/.test(item)) {
+      alphabets.push(item);
+      if (item === item.toLowerCase()) {
+        if (!highestLowercase || item > highestLowercase) {
+          highestLowercase = item;
+        }
+      }
+    }
+  });
+
+
+  let fileValid = false;
+  let fileMimeType = '';
+  let fileSizeKb = 0;
+
+  if (file_b64) {
+    fileValid = true; 
+    fileMimeType = 'application/octet-stream'; 
+    fileSizeKb = (Buffer.from(file_b64, 'base64').length / 1024).toFixed(2);
+  }
+
+  res.status(200).json({
+    is_success: true,
+    user_id: 'john_doe_17091999',
+    email: 'john@xyz.com',
+    roll_number: 'RA2111004010123',
+    numbers,
+    alphabets,
+    highest_lowercase_alphabet: highestLowercase ? [highestLowercase] : [],
+    file_valid: fileValid,
+    file_mime_type: fileMimeType,
+    file_size_kb: fileSizeKb
+  });
+});
+
+app.get('/bfhl', (req, res) => {
+  res.status(200).json({
+    operation_code: 1
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
